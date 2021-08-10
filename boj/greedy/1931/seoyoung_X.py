1df7cad0fd40e8d94edb4d4a4a3584e5f26d42cd
tjdud1199 <tjdud1199@naver.com>
2021-08-10 15:27:59 +0900

    sy exam greedy 01 02 and boj greedy retry

--- boj/greedy/1931/seoyoung_X.py
+++ boj/greedy/1931/seoyoung_X.py
@@ -1,12 +1,25 @@
-### 회의실 배정
-## 한 개의 회의실. 사용하고자 하는 n개의 회의. 회의실 사용표 만들기!
-## 각 회의에 대해 시작시간과 끝나는 시간 있음. 각 회의 겹치지 않게 하면서 회의실 사용할 수 있는 회의의 최대 개수
+'''
+< 회의실 배정 >
+각 회의에 대해 시작시간과 끝나는 시간이 있을 때, 각 회의 겹치지 않게 하면서 회의실 사용할 수 있는 회의의 최대 개수를 구하라!
+시작시간이 빠른 것 부터 넣는게 유리. 시작시간이 같으면 빨리 끝나는 순서대로 정렬
+'''
 
-n = int(input())   # 회의의 수
+n = int(input())
 meeting = []
-cnt = 0
 
 for i in range(n):
-    info = list(map(int, input().split()))
-    meeting.append(info)
+    s, e = map(int, input().split())
+    meeting.append((s, e))
 
+meeting.sort(key=lambda x:x[0]) # 시작시간으로 정렬
+meeting.sort(key=lambda x:x[1]) # 끝나는 시간으로 정렬
+
+end_time = 0
+result = 0
+
+for i, j in meeting:
+    if i >= end_time:
+        result += 1
+        end_time = j
+
+print(result)
